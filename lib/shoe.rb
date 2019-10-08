bf15493363598011350828c11d54e83d936038c4
Michelle Wong <michellekwong2@gmail.com>
2019-10-08 19:03:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    BRANDS.unique
   end
 
   def cobble
@@ -18,5 +19,5 @@ class Shoe
     @brand = brand
   end
   
-  
+ 
 end
